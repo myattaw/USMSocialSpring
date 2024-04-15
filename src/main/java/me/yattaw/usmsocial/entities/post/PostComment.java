369d99bcbc13ef9f1cfbcd4d76265ad18a5c8316
Abigail Pitcairn <abigail.pitcairn@maine.edu>
2024-04-15 18:09:41 -0400

    sample documentation
    This is how I document in my own projects. Please provide feedback on how these comments could be better fitted for this project. :)

--- src/main/java/me/yattaw/usmsocial/entities/post/PostComment.java
+++ src/main/java/me/yattaw/usmsocial/entities/post/PostComment.java
@@ -10,6 +10,10 @@ import me.yattaw.usmsocial.entities.user.UserPost;
 
 import java.time.LocalDateTime;
 
+/**
+ * Defines the properties of a <code>PostComment</code>.
+ * <code>PostComments</code> are stored in the table <code>usm_social_post_comments</code>.
+ */
 @Data
 @Entity
 @Builder
@@ -18,21 +22,39 @@ import java.time.LocalDateTime;
 @Table(name = "usm_social_post_comments")
 public class PostComment {
 
+    /**
+     * commentId - unique identifier for this comment
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
 
+    /**
+     * user - the user making this comment.
+     * cannot be nullified.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    /**
+     * post - the post this comment is added to.
+     * cannot be nullified.
+     */
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
     private UserPost post;
 
+    /**
+     * content - the contents of this comment.
+     * max length is 140 chars.
+     */
     @Column(length = 140)
     private String content;
 
+    /**
+     * timestamp - the time this comment was left.
+     */
     private LocalDateTime timestamp;
 
 }
--- src/main/java/me/yattaw/usmsocial/entities/post/PostLike.java
+++ src/main/java/me/yattaw/usmsocial/entities/post/PostLike.java
@@ -7,6 +7,10 @@ import me.yattaw.usmsocial.entities.user.UserPost;
 
 import java.time.LocalDateTime;
 
+/**
+ * Defines the properties of a <code>PostLike</code>.
+ * <code>PostLikes</code> are stored in the table <code>usm_social_post_likes</code>.
+ */
 @Data
 @Entity
 @Builder
@@ -16,18 +20,32 @@ import java.time.LocalDateTime;
 @Table(name = "usm_social_post_likes")
 public class PostLike {
 
+    /**
+     * likeId - unique identifier for this like.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long likeId;
 
+    /**
+     * user - the user leaving this like.
+     * cannot be nullified.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    /**
+     * post - the post this like is added to.
+     * cannot be nullified.
+     */
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
     private UserPost post;
 
+    /**
+     * timestamp - the time this like was added.
+     */
     private LocalDateTime timestamp;
 
 }
