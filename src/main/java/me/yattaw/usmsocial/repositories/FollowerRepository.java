c946a2d067b3006e0a1dfcc0369dbbc7a303cc1d
Owen Bean <owenbean@Owens-MBP-98.lan>
2024-04-17 19:02:24 -0400

    Fixed followers and followings being backwards

--- src/main/java/me/yattaw/usmsocial/repositories/FollowerRepository.java
+++ src/main/java/me/yattaw/usmsocial/repositories/FollowerRepository.java
@@ -16,22 +16,22 @@ public interface FollowerRepository extends JpaRepository<UserFollowers, UserFol
     Integer getUserFollowerEachOtherCount(@Param("followerId") Integer followerId, @Param("followingId") Integer followingId);
 
     @Query(
-        value = "SELECT following_id FROM usm_social_followers WHERE follower_id = :followerId",
+        value = "SELECT follower_id FROM usm_social_followers WHERE following_id = :followerId",
         nativeQuery = true)
     Page<Integer> getUserFollowers(@Param("followerId") Integer followerId, Pageable pageable);
 
     @Query(
-        value = "SELECT follower_id FROM usm_social_followers WHERE following_id = :followingId",
+        value = "SELECT following_id FROM usm_social_followers WHERE follower_id = :followingId",
         nativeQuery = true)
     Page<Integer> getUserFollowings(@Param("followingId") Integer followingId, Pageable pageable);
 
     @Query(
-        value = "SELECT COUNT(*) FROM usm_social_followers WHERE following_id = :followingId",
+        value = "SELECT COUNT(*) FROM usm_social_followers WHERE follower_id = :followingId",
         nativeQuery = true)
     Integer getCountFollowings(@Param("followingId") Integer followingId);
 
     @Query(
-        value = "SELECT COUNT(*) FROM usm_social_followers WHERE follower_id = :followerId",
+        value = "SELECT COUNT(*) FROM usm_social_followers WHERE following_id = :followerId",
         nativeQuery = true)
     Integer getCountFollowers(@Param("followerId") Integer followerId);
 }
